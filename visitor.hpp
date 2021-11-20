34eabfbdd0235191789e968effbfa852108b35a2
blake2703 <dickersonb27@yahoo.com>
2021-11-19 22:32:41 -0800

    Visitor abstract class

--- visitor.hpp
+++ visitor.hpp
@@ -3,11 +3,11 @@
 
 class Op;
 class Rand;
-class Add;
-class Sub;
-class Mult;
+class add;
+class sub;
+class mult;
 class Div;
-class Pow;
+class exponent;
 
 class Visitor{
     public:
@@ -21,21 +21,21 @@ class Visitor{
         // index = 0 -> begin
         // index = 1 -> middle
         // index = 2 -> end
-        virtual void visit_add_begin(Add* node) = 0;
-        virtual void visit_add_middle(Add* node) = 0;
-        virtual void visit_add_end(Add* node) = 0;
-        virtual void visit_sub_begin(Sub* node) = 0;
-        virtual void visit_sub_middle(Sub* node) = 0;
-        virtual void visit_sub_end(Sub* node) = 0;
-        virtual void visit_mult_begin(Mult* node) = 0;
-        virtual void visit_mult_middle(Mult* node) = 0;
-        virtual void visit_mult_end(Mult* node) = 0;
+        virtual void visit_add_begin(add* node) = 0;
+        virtual void visit_add_middle(add* node) = 0;
+        virtual void visit_add_end(add* node) = 0;
+        virtual void visit_sub_begin(sub* node) = 0;
+        virtual void visit_sub_middle(sub* node) = 0;
+        virtual void visit_sub_end(sub* node) = 0;
+        virtual void visit_mult_begin(mult* node) = 0;
+        virtual void visit_mult_middle(mult* node) = 0;
+        virtual void visit_mult_end(mult* node) = 0;
         virtual void visit_div_begin(Div* node) = 0;
         virtual void visit_div_middle(Div* node) = 0;
         virtual void visit_div_end(Div* node) = 0;
-        virtual void visit_pow_begin(Pow* node) = 0;
-        virtual void visit_pow_middle(Pow* node) = 0;
-        virtual void visit_pow_end(Pow* node) = 0;
+        virtual void visit_pow_begin(exponent* node) = 0;
+        virtual void visit_pow_middle(exponent* node) = 0;
+        virtual void visit_pow_end(exponent* node) = 0;
 };
 
 #endif //__VISITOR_HPP__
